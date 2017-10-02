2c59dcb53cbd41a06e0dfd286d873d33a8047b85
onurgzr44 <32458595+onurgzr44@users.noreply.github.com>
2017-10-02 20:12:08 +0300

    Birinci_Örnek

---
+++ BirinciÖrnek.cs
@@ -0,0 +1,22 @@
+using System; 
+
+namespace omer{
+
+   class BirinciOrnek{
+
+   		static void Main(String[] args)
+   		{
+   			string ad, soyad ; 
+   			ad = "Onur"; 
+   			soyad = "Gezer";
+
+   			Console.WriteLine(ad + " " + soyad); 
+
+   			Console.ReadKey(); 
+
+   		}
+
+
+   }
+
+}
